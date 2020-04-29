a79a1b1160c582227a0c24e4f96bc2d846ff4b93
Rodolfo Marriel <rodolfo.29r@gmail.com>
2020-04-29 19:36:51 -0300

    estilizando página principal

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,86 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 // React.FC => React Function Component
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="repository">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
+            alt="Rodolfo Marriel"
+          />
+          <div>
+            <strong>Projeto</strong>
+            <p>Algum projeto</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="repository">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
+            alt="Rodolfo Marriel"
+          />
+          <div>
+            <strong>Projeto</strong>
+            <p>Algum projeto</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="repository">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
+            alt="Rodolfo Marriel"
+          />
+          <div>
+            <strong>Projeto</strong>
+            <p>Algum projeto</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="repository">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
+            alt="Rodolfo Marriel"
+          />
+          <div>
+            <strong>Projeto</strong>
+            <p>Algum projeto</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="repository">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
+            alt="Rodolfo Marriel"
+          />
+          <div>
+            <strong>Projeto</strong>
+            <p>Algum projeto</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
