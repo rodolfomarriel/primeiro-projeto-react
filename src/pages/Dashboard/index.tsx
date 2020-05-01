ef71b2a6577efe6423c544e8c54f3d610842487f
Rodolfo Marriel <rodolfo.29r@gmail.com>
2020-04-30 22:52:32 -0300

    implementando css e chamada de informações da api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,83 +1,81 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './style';
+import { Title, Form, Repositories, Error } from './style';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 // React.FC => React Function Component
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    console.log(newRepo);
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(eventNewRepo) => setNewRepo(eventNewRepo.target.value)}
+          placeholder="Digite o nome do Repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="repository">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
-            alt="Rodolfo Marriel"
-          />
-          <div>
-            <strong>Projeto</strong>
-            <p>Algum projeto</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="repository">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="repository">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
-            alt="Rodolfo Marriel"
-          />
-          <div>
-            <strong>Projeto</strong>
-            <p>Algum projeto</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="repository">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
-            alt="Rodolfo Marriel"
-          />
-          <div>
-            <strong>Projeto</strong>
-            <p>Algum projeto</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="repository">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
-            alt="Rodolfo Marriel"
-          />
-          <div>
-            <strong>Projeto</strong>
-            <p>Algum projeto</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="repository">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13751617?s=460&u=82baf0ba4c80683321617710f7201aaa23134f98&v=4"
-            alt="Rodolfo Marriel"
-          />
-          <div>
-            <strong>Projeto</strong>
-            <p>Algum projeto</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
